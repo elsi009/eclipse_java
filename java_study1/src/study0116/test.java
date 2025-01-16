6b9ba99810160ab688faaa356a21fda4b8f3412f
dw-014 <dw-014@DESKTOP-0CHCG09>
2025-01-16 12:49:48 +0900

    0116 test

--- java_study1/src/study0116/test.java
+++ java_study1/src/study0116/test.java
@@ -4,7 +4,23 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		/*
+		 *  여러줄 주석 
+		 *  저장 단축키 - ctrl + s
+		 *  주석 단축키 - ctrl + /
+		 *  실행 단축키 - ctrl + F11
+		 */
+		System.out.println("자바 이클립스 테스트");
+		
+		// 정수 : int, byte, short, long
+		// 실수 : float , double
+		// 문자 : char
+		// 논리 : boolean
+		int age;
+		age = 100; 
+		age = 27;
+		age = '가'; // 자바에서 ''표현은 문자를 의미
+				   // "" 표현은 문자열을 의미
 	}
 
 }
