e3f61eb378235d2aa22e58c3ac5c0ddadc7efcb9
82104 <82104@192.168.219.100>
2025-01-18 21:41:40 +0900

    homework

---
+++ test/src/test/test1.java
@@ -0,0 +1,22 @@
+package test;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int cigrat=3;
+		int cigMinute= cigrat * 2 * 365 * 20;
+		//double cigHour = cigMinute / 60;
+		//double cigDay = cigHour / 24 ;
+		int cigHour = cigMinute / 60;
+		int cigDay = cigHour / 24 ;
+		
+		System.out.println(cigMinute);
+		System.out.println(cigHour);
+		System.out.println(cigDay);
+		
+		
+	}
+
+}
