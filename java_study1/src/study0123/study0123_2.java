e3ac11c18512fef1dcbbbc749487a08bb937a4cf
elsi009 <pengrung3512@gmail.com>
2025-01-24 12:47:57 +0900

    24

--- java_study1/src/study0123/study0123_2.java
+++ java_study1/src/study0123/study0123_2.java
@@ -7,6 +7,10 @@ public class study0123_2 {
 		System.out.println(27 & 13); // and 연산
 		System.out.println(27 | 13); // or 연산
 		System.out.println(27 ^ 13); // xor 연산
+		System.out.println(~13);
+		
+		System.out.println(21<<2);
+		System.out.println(17>>3);
 	}
 
 }
---
+++ java_study1/src/study0124/study0124_1.java
@@ -0,0 +1,63 @@
+package study0124;
+
+import java.util.Scanner;
+
+public class study0124_1 {
+
+	public static void main(String[] args) {
+//		int tall=163;
+//		
+//		if(tall>=140) {
+//			System.out.println("탑승가능");
+//			System.out.println("키가 140이상이다.");
+//			int a= 10;
+//			a=132+2;
+//		} else {
+//			System.out.println("탑승 불가능");
+//		}
+//		
+//		int num1=35, num2=12;
+//		
+//		if(num1>num2) {
+//			System.out.println("num1이 더큽니다");
+//		}
+//		else {
+//			System.out.println("num2가 더큽니다.");
+//		}
+//		
+//		int 철수=275, 민수 = 260;
+//		
+//		if(철수>민수){
+//			System.out.println("철수가 크다");
+//		}
+//		else{
+//			System.out.println("민수가 크다");
+//		}
+//		int money = 4530;
+//		if( money<5200) {
+//			int m = 5200-money;
+//			System.out.println(m);
+//		}
+//		else {
+//			System.out.println("이체 필요 없음");
+//		}
+//		Scanner scan = new Scanner(System.in);
+//		int tall = scan.nextInt();
+//		if(tall>=140 && tall<=190) {
+//			System.out.println("탑승 가능");
+//		}
+//		else {
+//			System.out.println("탑승 불가");
+//		}
+		int tall = 160;
+		int wg = 60;
+		if( tall <130 && wg <45) {
+			System.out.println("탑승불가");
+		} else
+		{
+			System.out.println("탑승가능");
+		}
+		
+	}
+
+}
